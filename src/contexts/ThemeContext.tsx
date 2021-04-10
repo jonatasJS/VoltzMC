515f5be44d21ef4b846e9f2c5cfa326c3f254a7f
Jônatas Souza Soares <56240097+sozinhoL@users.noreply.github.com>
2021-04-09 23:05:40 -0300

    Update ThemeContext.tsx

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -18,7 +18,7 @@ interface ThemeProviderProps {
 export const ThemeContext = createContext({} as ThemeContextData);
 
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('light');
 
   function toggleTheme() {
     if (theme === 'light') {
