558bae230a547dd0e439070cd31ea2c1c236b466
Jônatas Souza Soares <56240097+sozinhoL@users.noreply.github.com>
2021-04-09 22:42:18 -0300

    Update styles.ts

--- src/pages/shop/styles.ts
+++ src/pages/shop/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export default const Container = styled.div`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
