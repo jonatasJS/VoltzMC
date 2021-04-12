7f3d73cfb65e74ad9cd352028153098a3e697686
sozinhoL <soaresjonatas398@gmail.com>
2021-04-11 23:28:02 -0300

    Add test paga ADM a new head

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useSession } from 'next-auth/client';
+import Head from 'next/head';
+
 import LoginInterface from '../../components/LoginInterface';
 
 export default function Admin() {
@@ -7,6 +9,10 @@ export default function Admin() {
   
   return (
     <div>
+      <Head>
+        <title>Administracao | VoltzMC</title>
+      </Head>
+      
       {loading ? <h1>Carregando...</h1> : <div>{session ? <h1>Vc `e um ADM!! Que legal :)</h1> : <LoginInterface />}</div>}
     </div>
   );
