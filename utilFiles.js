3f0dc6c66a9a40b54e9a6650d336d22256477bea
sozinhoL <soaresjonatas398@gmail.com>
2021-04-18 23:16:57 -0300

    Deleted utilFiles.js

--- utilFiles.js
+++
@@ -1,8 +0,0 @@
-const util = require('minecraft-server-util');
-
-const data = util.status('jogar.voltzmc.com.br').then(e => {
-  console.log(e.data);
-  return e.data;
-});
-
-module.exports = data;
