5783792ef3b3518ca1ad3112cd6bbae2bcec3637
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2021-06-22 12:58:54 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,8 +11,7 @@ export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-    const styles = `.pace{-webkit-pointer-events:none;pointer-events:none;-webkit-user-select:none;-moz-user-select:none;user-select:none}.pace-inactive{display:none}.pace .pace-progress{background:rgb(255, 170, 0);;position:fixed;z-index:2000;top:0;right:100%;width:100%;height:2px}`
-
+    
     try {
       ctx.renderPage = () =>
         originalRenderPage({
@@ -45,7 +44,7 @@ export default class MyDocument extends Document {
             type="image/png"
           />
           <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css"/>
+          <link rel="stylesheet" href="https://raw.githubusercontent.com/jonatasJS/VoltzMC/main/loadingBar.css"/>
           <style>
             {styles}
           </style>
