3ac564aa40a72485902e25a5126f06e3bdae3446
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2021-06-22 12:53:27 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
+    const styles = `.pace{-webkit-pointer-events:none;pointer-events:none;-webkit-user-select:none;-moz-user-select:none;user-select:none}.pace-inactive{display:none}.pace .pace-progress{background:rgb(255, 170, 0);;position:fixed;z-index:2000;top:0;right:100%;width:100%;height:2px}`
 
     try {
       ctx.renderPage = () =>
@@ -46,12 +47,7 @@ export default class MyDocument extends Document {
           <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css"/>
           <style>
-           /*!
-           * pace.js v1.2.4 | Default theme
-           * https://github.com/CodeByZach/pace/
-           * Licensed MIT © HubSpot, Inc.
-           */
-          .pace{-webkit-pointer-events:none;pointer-events:none;-webkit-user-select:none;-moz-user-select:none;user-select:none}.pace-inactive{display:none}.pace .pace-progress{background:rgb(255, 170, 0);;position:fixed;z-index:2000;top:0;right:100%;width:100%;height:2px}
+            {styles}
           </style>
           <link
             rel="preconnect"
