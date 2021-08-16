93a754102a343bfaf350e89eed8ab6deaaf1134a
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2021-08-16 12:37:56 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    reactStrictMode: false,
+    webpack5: true,
+    images: {
+        domains: ['github.com', 'localhost', 'avatars.githubusercontent.com', 'cdn.discordapp.com', 'pagead2.googlesyndication.com'],
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+      },
+}
