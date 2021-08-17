9c350047b6bb54743ab2c63c43302d735c6a2773
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2021-08-17 14:45:41 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -65,8 +65,8 @@ export default class MyDocument extends Document {
           <NextScript />
 
           <script src="https://www.paypalobjects.com/api/checkout.js" />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4515639184646084" crossOrigin="anonymous" />
           
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4515639184646084" crossOrigin="anonymous" />
            <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
           </script>
